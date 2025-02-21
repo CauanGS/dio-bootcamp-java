398317b19792acd7f3598de3cb6e225abdbc6b92
cauan <cauangabrieldesouza@gmail.com>
2025-02-21 14:16:07 -0300

    Primeiro commit

---
+++ controle-candidatos/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Processo Seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido){
+            System.out.println("Aguardando resultado para os demais candidato");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    }
+}
